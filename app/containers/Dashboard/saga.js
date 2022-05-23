2d79262ea3e75a779d93e96c77e0b23fd13e7f29
Gaurav1881 <39128612+Gaurav1881@users.noreply.github.com>
2022-05-23 01:17:51 -0400

    adding logs

--- app/containers/Dashboard/saga.js
+++ app/containers/Dashboard/saga.js
@@ -3,43 +3,43 @@ import moment from 'moment';
 import { call, put, takeLatest } from 'redux-saga/effects';
 import _isNumber from 'lodash/isNumber';
 import _isEmpty from 'lodash/isEmpty';
-import request from 'utils/request';
 import { GET_LOGS } from './constants';
 
+import request from 'utils/request';
 import { setServiceLogs } from './actions';
 
 export function* attemptGetLogs(action) {
-  const date = moment().format('YYYY-MM-DD');
+    let date = moment().format('YYYY-MM-DD');
 
-  const { API_URL } = process.env;
+    const API_URL = `https://carwash-pos-backend.vercel.app`;
 
-  const requestUrl = `${API_URL}/api/logs?date=${encodeURIComponent(date)}`;
+    const requestUrl = `${API_URL}/api/logs?date=` + encodeURIComponent(date);
 
-  try {
-    const options = {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      mode: 'cors',
-      Cache: 'no-cache',
-    };
-    const result = yield call(request, requestUrl, options);
+    try {
+        const options = {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            mode: 'cors',
+            'Cache': 'no-cache'
+        };
+        let result = yield call(request, requestUrl, options);
 
-    results = results.sort((a, b) => {
-      moment(a.date, 'YYYY-MM-DD').isAfter(moment(b.date, 'YYYY-MM-DD'));
-    });
+        results = results.sort((a, b) => {
+            moment(a.date, 'YYYY-MM-DD').isAfter(moment(b.date, 'YYYY-MM-DD'))
+        })
 
-    console.log(result);
-    yield put(setServiceLogs(result));
-  } catch (err) {
-    console.log(err);
-  }
+        console.log(result);
+        yield put(setServiceLogs(result));
+    } catch (err) {
+        console.log(err);
+    }
 }
 
 /**
  * Root saga manages watcher lifecycle
  */
 export default function* logSaga() {
-  yield takeLatest(GET_LOGS, attemptGetLogs);
+    yield takeLatest(GET_LOGS, attemptGetLogs);
 }
--- app/containers/ServiceSelection/saga.js
+++ app/containers/ServiceSelection/saga.js
@@ -1,35 +1,38 @@
 import { call, put, takeLatest } from 'redux-saga/effects';
 import _isNumber from 'lodash/isNumber';
 import _isEmpty from 'lodash/isEmpty';
-import request from 'utils/request';
 import { ADD_TO_LOGS } from './constants';
 
+import request from 'utils/request';
+
 export function* attemptPushToDB(action) {
-  const { API_URL } = process.env;
 
-  const requestUrl = `${API_URL}/api/logs`;
+    const API_URL = `https://carwash-pos-backend.vercel.app`;
 
-  const log = action.payload;
-  console.log(action.payload);
-  try {
-    const options = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        log,
-      }),
-      mode: 'cors',
-      Cache: 'no-cache',
-    };
-    const result = yield call(request, requestUrl, options);
-  } catch (err) {}
+    const requestUrl = `${API_URL}/api/logs`;
+
+    const log = action.payload;
+    console.log(action.payload);
+    try {
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                log,
+            }),
+            mode: 'cors',
+            'Cache': 'no-cache'
+        };
+        const result = yield call(request, requestUrl, options);
+    } catch (err) {
+    }
 }
 
 /**
  * Root saga manages watcher lifecycle
  */
 export default function* logSaga() {
-  yield takeLatest(ADD_TO_LOGS, attemptPushToDB);
+    yield takeLatest(ADD_TO_LOGS, attemptPushToDB);
 }
